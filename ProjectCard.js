e438d2844fb5c4b243eaa8db4fc069d04f88bbc3
abechoi <abraham.choi@icloud.com>
2020-07-30 12:27:18 -0700

    Routing fixed

---
+++ ProjectCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const ProjectCard = ({project}) => {
+
+  return (
+    <Link to={project.url}>
+      <li className="project-card" key={project.id}>
+        <h3>{ project.title }</h3>
+        <p>{ project.createdAt }</p>
+        <p>{ project.info }</p>
+      </li>
+    </Link>
+  )
+}
+
+export default ProjectCard;
